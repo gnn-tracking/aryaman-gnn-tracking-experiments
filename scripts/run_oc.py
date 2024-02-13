0307611f0770ad57ca5726bd9a5076dc5eebf02c
Aryaman Jeendgar <aj2239@della8.princeton.edu>
2024-02-13 05:08:16 -0500

    SLURM scripts

---
+++ scripts/run_oc.py
@@ -0,0 +1,58 @@
+import torch
+import wandb
+from gnn_tracking.training.callbacks import ExpandWandbConfig, PrintValidationMetrics
+from gnn_tracking.utils.loading import TrackingDataModule
+from gnn_tracking.utils.nomenclature import random_trial_name
+from lightning_fabric.plugins.environments.slurm import SLURMEnvironment
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichProgressBar
+from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from wandb_osh.lightning_hooks import TriggerWandbSyncLightningCallback
+
+name = random_trial_name()
+
+
+logger = WandbLogger(
+    project="aryaman-gnn-experiments",
+    group="noise-classification-oc",
+    offline=True,
+    version=name,
+    tags=["ground-truth-noise-pixel-oc-run2"],
+)
+
+# Make sure that wandb init is called
+_ = logger.experiment
+wandb.define_metric(
+    "max_trk.double_majority_pt0.9",
+    step_metric="trk.double_majority_pt0.9",
+    summary="max",
+)
+
+tb_logger = TensorBoardLogger(".", version=name)
+
+
+def cli_main():
+    torch.set_float32_matmul_precision("medium")
+
+    # noinspection PyUnusedLocal
+    cli = LightningCLI(  # noqa: F841
+        datamodule_class=TrackingDataModule,
+        trainer_defaults={
+            "callbacks": [
+                RichProgressBar(leave=True),
+                TriggerWandbSyncLightningCallback(),
+                PrintValidationMetrics(),
+                ExpandWandbConfig(),
+                EarlyStopping(monitor="total", mode="min", patience=50),
+                ModelCheckpoint(
+                    save_top_k=2, monitor="trk.double_majority_pt0.9", mode="max"
+                ),
+            ],
+            "logger": [tb_logger, logger],
+            "plugins": [SLURMEnvironment(auto_requeue=False)],
+        },
+    )
+
+
+if __name__ == "__main__":
+    cli_main()
